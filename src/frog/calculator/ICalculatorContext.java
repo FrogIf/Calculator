9c88c3d346081f42b032f07f8170941ebaecf857
frogif <skyfrog@163.com>
2020-03-14 21:41:50 +0800

    decimal to frag

--- src/frog/calculator/ICalculatorContext.java
+++ src/frog/calculator/ICalculatorContext.java
@@ -2,6 +2,9 @@ package frog.calculator;
 
 import frog.calculator.util.collection.IList;
 
+/**
+ * 计算器上下文
+ */
 public interface ICalculatorContext {
 
     void addCalculateListener(ICalculateListener calculateListener);
--- src/frog/calculator/ICalculatorManager.java
+++ src/frog/calculator/ICalculatorManager.java
@@ -5,8 +5,16 @@ import frog.calculator.build.resolve.IResolverResult;
 import frog.calculator.connect.ICalculatorSession;
 import frog.calculator.express.IExpression;
 
+/**
+ * 计算器管理器
+ */
 public interface ICalculatorManager {
 
+    /**
+     * 将表达式包装为解析结果
+     * @param expression 待包装的表达式
+     * @return 解析结果
+     */
     IResolverResult createResolverResult(IExpression expression);
 
     ICalculatorSession createCalculatorSession();
